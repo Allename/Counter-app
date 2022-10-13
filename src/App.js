9f01db9ae39868a67d3f70a5eebf3f1e47dcb34f
Allename <allename.dev@gmail.com>
2022-10-13 16:24:49 +0100

    imported {useState} hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-
+      <h1 className='text'>0</h1>
     </div>
   );
 }
