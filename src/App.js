c5396192762ec54c8f01deed6c3bde4f378fa20e
Allename <allename.dev@gmail.com>
2022-10-13 16:57:26 +0100

    added functionality

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ function App() {
 
   const [counter, setCounter] = useState(0);
 
+
   return (
     <div className="App">
       <h1 className='counter-label'>Count: {counter}</h1>
 
       <div className="button-container">
-        <button>Increase</button>
-        <button className='reset'>Reset</button>
-        <button>Decrease</button>
+        <button onClick={() => {setCounter(counter + 1)}}>Increase</button>
+        <button onClick={() => {setCounter(0)}} className='reset'>Reset</button>
+        <button onClick={() => {
+          if(counter === 0) {
+            return;
+          } else {
+            setCounter(counter - 1)
+          }
+        }}>Decrease</button>
       </div>
     </div>
   );
